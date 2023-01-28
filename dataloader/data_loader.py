dcde2900b943475c73a6ecd565f837b9059fb2b7
cwghnu <cwg@hnu.edu.cn>
2023-01-28 18:19:49 +0800

    Add training code

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -5,44 +5,65 @@ import random
 import numpy as np
 from utils.kaldi_data import KaldiData
 
+def _count_frames(data_len, size, step):
+    # no padding at edges, last remaining samples are ignored
+    return int((data_len - size + step) / step)
+
 class Dataset(torch.utils.data.Dataset):
     """
     This is the main class that calculates the spectrogram and returns the
     spectrogram, audio pair.
     """
-    def __init__(self, path_dataset, mfcc_config, nframes=40):
+    def __init__(self, path_dataset, mfcc_config, chunk_size=2000, max_speakers=4):
 
         self.kaldi_obj = KaldiData(path_dataset)
         self.mfcc_dir = os.path.join(path_dataset, "mfcc")
         self.ivec_dir = os.path.join(path_dataset, "ivec")
-        
-        self.utt2feat   = files_to_dict(os.path.join(training_dir,'feats.scp'))
-        self.utt2nframe = files_to_dict(os.path.join(training_dir,'utt2num_frames'))       
-        self.utt2target = files_to_dict(os.path.join(training_dir,'dense_targets.scp'))
-        self.utt2iv     = files_to_dict(os.path.join(training_dir,'ivector_online.scp'))
-        self.utt_list   = [k for k in self.utt2target.keys() if int(self.utt2nframe[k]) >= nframes ]
+        self.label_dir = os.path.join(path_dataset, "label")
         
         self.nframes = nframes
-
-    def __getitem__(self, index):
-        utt         = self.utt_list[index]
-        feat_length = int(self.utt2nframe[utt])
-
-        assert feat_length >= self.nframes
-
-        feat        = load_scp_to_torch(self.utt2feat[utt]).unsqueeze(0)
-        target      = load_scp_to_torch(self.utt2target[utt])[:, 1::2]
-        ivectors    = load_scp_to_torch(self.utt2iv[utt]).mean(dim=0)
+        self.rate = mfcc_config["sampling_rate"]
+        self.frame_len = mfcc_config["frame_length"] * self.rate
+        self.frame_shift = mfcc_config["frame_shift"] * self.rate
+        self.subsampling = 1
+        self.chunk_size = chunk_size
+        self.max_speakers = 4
         
-        max_start  = feat_length - self.nframes
-        feat_start = random.randint(0, max_start)
-        feat       = feat[:, feat_start:(feat_start+self.nframes)]
-        target     = target[feat_start:(feat_start+self.nframes)]
+        self.total_chunk = 0
+        for rec in self.kaldi_obj.wavs:
+            num_frames = _count_frames(self.kaldi_obj.reco2dur[rec]*self.rate, self.frame_len, self.frame_shift)
+            num_chunks = num_frames // self.frame_lenchunk_size
+            self.total_chunk += num_chunks
+        self.total_chunk *= 2
+        print("[Dataset Msg] total number of chunks: {}".format(self.total_chunk))
 
-        return feat, target, ivectors
+    @lru_cache
+    def __getitem__(self, index):
+        
+        idx_utt = index % len(self.kaldi_obj.wavs)
+        utt_id = self.kaldi_obj.wavs[idx_utt]
+        
+        mfcc_utt = np.load(os.path.join(self.mfcc_dir, utt_id + '.npy'))    # [num_frames, 72]
+        ivec_utt = np.load(os.path.join(self.ivec_dir, utt_id + '.npy'))    # [num_speakers, 400]
+        label_utt = np.load(os.path.join(self.label_dir, utt_id + '.npy'))  # [num_frames, num_speakers]
+        
+        assert len(mfcc_utt) == len(label_utt)
+        assert len(ivec_utt) == label_utt.shape[1]
+        
+        max_start  = len(mfcc_utt) - self.chunk_size
+        idx_start = random.randint(0, max_start)
+        
+        mfcc_utt = mfcc_utt[idx_start:(idx_start+self.chunk_size)]
+        label_utt = label_utt[idx_start:(idx_start+self.chunk_size)]
+
+        return {
+            "mfcc": mfcc_utt,
+            "label": label_utt,
+            "ivector": ivec_utt,
+        }
     
     def __len__(self):
-        return len(self.utt_list)
+        return self.total_chunk
 
 
 class EvalDataset(torch.utils.data.Dataset):
---
+++ train.py
@@ -0,0 +1,186 @@
+import os
+import sys
+sys.path.insert(0,os.getcwd())
+
+import time
+import torch
+import logging
+import numpy as np
+from pathlib import Path
+from importlib import import_module
+import json
+
+from torch.utils.data import DataLoader
+from util.dataset_loader import Dataset
+
+def collate_fn(batches):
+    mfcc_batches = [item['mfcc'] for item in batches]
+    label_batches = [item['label'] for item in batches]
+    ivector_batches = [item['ivector'] for item in batches]
+    
+    mfcc_batches = torch.stack(mfcc_batches)
+    label_batches = torch.stack(label_batches)
+    ivector_batches = torch.stack(ivector_batches)
+    
+    egs = {
+        'mfcc': mfcc_batches,
+        'label': label_batches,
+        "ivector": ivector_batches,
+    }
+    
+    return egs
+
+def train(train_config): 
+    # Initial
+    output_directory     = train_config.get('output_directory', '')
+    max_iter             = train_config.get('max_iter', 100000)
+    batch_size           = train_config.get('batch_size', 128)
+    nframes              = train_config.get('nframes', 40)
+    iters_per_checkpoint = train_config.get('iters_per_checkpoint', 10000)
+    iters_per_log        = train_config.get('iters_per_log', 1000)
+    seed                 = train_config.get('seed', 1234)
+    checkpoint_path      = train_config.get('checkpoint_path', '')
+    trainer_type         = train_config.get('trainer_type', 'basic')
+    epochs_per_eval      = train_config.get('epochs_per_eval', 5)
+
+    # Setup
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)   
+
+    # Initial trainer
+    module = import_module('trainer.{}'.format(trainer_type), package=None)
+    TRAINER = getattr( module, 'Trainer')
+    trainer = TRAINER( train_config, model_config)
+
+    # Load checkpoint if the path is given 
+    iteration = 1
+    epoch = 0
+    if checkpoint_path != "":
+        iteration = trainer.load_checkpoint( checkpoint_path)
+        iteration += 1  # next iteration is iteration + 1
+        
+    with open(train_config['mfcc_config']) as f:
+        data = f.read()
+    mfcc_config = json.loads(data)
+    mfcc_config = mfcc_config["mfcc_config"]
+
+    # Load training data
+    trainset = Dataset(train_config['training_dir'], mfcc_config, chunk_size=nframes)    
+    train_loader = DataLoader(trainset, num_workers=32, shuffle=True,
+                              batch_size=batch_size,
+                              pin_memory=True,
+                              drop_last=True,
+                              collate_fn=collate_fn)
+    
+    # Load evaluation data
+    evalset = Dataset(train_config['eval_dir'], mfcc_config, chunk_size=nframes)    
+    eval_loader = DataLoader(evalset, num_workers=32, shuffle=True,
+                              batch_size=batch_size,
+                              pin_memory=True,
+                              drop_last=True,
+                              collate_fn=collate_fn)
+
+    # Get shared output_directory ready
+    output_directory = Path(output_directory)
+    output_directory.mkdir(parents=True, exist_ok=True)
+    
+    # Prepare logger
+    logger = logging.getLogger("logger")
+    handler1 = logging.StreamHandler()
+    handler2 = logging.FileHandler(filename=str(output_directory/'Stat'))
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s %(message)s",
+                                  datefmt="%m-%d %H:%M:%S")
+    handler1.setFormatter(formatter)
+    handler2.setFormatter(formatter)
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+
+    logger.info("Output directory: {}".format(output_directory))
+    logger.info("Training utterances: {}".format(len(trainset)))
+    logger.info("Batch size: {}".format(batch_size))
+    logger.info("# of frames per sample: {}".format(nframes))
+
+    # ================ MAIN TRAINNIG LOOP! ===================
+    
+    logger.info("Start traininig...")
+
+    loss_log = dict()
+    while iteration <= max_iter:
+        for i, batch in enumerate(train_loader):
+            
+            iteration, loss_detail, lr = trainer.step(batch, iteration=iteration)
+
+            # Keep Loss detail
+            for key,val in loss_detail.items():
+                if key not in loss_log.keys():
+                    loss_log[key] = list()
+                loss_log[key].append(val)
+            
+            # Save model per N iterations
+            if iteration % iters_per_checkpoint == 0:
+                checkpoint_path =  output_directory / "{}_{}".format(time.strftime("%m-%d_%H-%M", time.localtime()),iteration)
+                trainer.save_checkpoint( checkpoint_path)
+
+            # Show log per M iterations
+            if iteration % iters_per_log == 0 and len(loss_log.keys()) > 0:
+                mseg = 'Iter {}:'.format( iteration)
+                for key,val in loss_log.items():
+                    mseg += '  {}: {:.6f}'.format(key,np.mean(val))
+                mseg += '  lr: {:.6f}'.format(lr)
+                logger.info(mseg)
+                loss_log = dict()
+
+            if iteration > max_iter:
+                break
+            
+        epoch += 1
+        if epoch % epochs_per_eval == 0:
+            der_list = []
+            trainer.model.eval()
+            for i, batch in enumerate(eval_loader):
+                preds = trainer.model.inference(batch).squeeze(0).cpu().numpy()
+        
+
+    print('Finished')
+        
+
+if __name__ == "__main__":
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, default='tsvad_config.json',
+                        help='JSON file for configuration')
+    parser.add_argument('-o', '--output_directory', type=str, default=None,
+                        help='Directory for checkpoint output')
+    parser.add_argument('-p', '--checkpoint_path', type=str, default=None,
+                        help='checkpoint path to keep training')
+    parser.add_argument('-T', '--training_dir', type=str, default=None,
+                        help='Traininig dictionary path')
+
+    parser.add_argument('-g', '--gpu', type=str, default='0',
+                        help='Using gpu #')
+    args = parser.parse_args()
+
+    # Parse configs.  Globals nicer in this case
+    with open(args.config) as f:
+        data = f.read()
+    config = json.loads(data)
+    train_config = config["train_config"]
+    global model_config
+    model_config = config["model_config"]
+
+    if args.output_directory is not None:
+        train_config['output_directory'] = args.output_directory
+    if args.checkpoint_path is not None:
+        train_config['checkpoint_path'] = args.checkpoint_path
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = False
+
+    train(train_config)
